745c9e49b429b3d6d696427ae94b0830ddd9640d
MarkisPaulo <markispaulo.atanasio3@gmail.com>
2025-10-14 10:51:45 -0300

    tela de listar não aparece os icones

--- cliente-Listar.php
+++ cliente-Listar.php
@@ -12,37 +12,40 @@ $resultado = mysqli_query($conexao, $sql);
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listagem</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-</head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/listar.css">
+
 <body>
 
-    <?php require_once("menu.php"); ?>
+    <?php require_once("header.php"); ?>
+
+    <?php if (isset($mensagem)) { ?>
+        <div class="alert" role="alert">
+            <?= $mensagem ?>
+        </div>
+    <?php } ?>
+
     <div class="container">
 
-        <?php if (isset($mensagem)) { ?>
-            <div class="alert alert-success mt-3" role="alert">
-                <?= $mensagem ?>
-            </div>
-        <?php } ?>
-
-        <div class="card mt-3">
+        <div class="card">
             <div class="card-body">
-                <h5 class="card-title">
+                <h5 class="title">
                     Listagem de Clientes
-                    <a href="cliente-cadastrar.php" class="btn btn-primary"><i class="bi bi-plus-circle"></i></a>
+                    <a href="cliente-cadastrar.php">
+                        <i class="fas fa-solid fa-circle-plus"></i> Novo Cliente
+                    </a>
                 </h5>
             </div>
         </div>
 
-        <table class="table table-striped mt-3">
+        <table class="table-container">
             <thead>
                 <tr>
                     <th scope="col">Código</th>
@@ -57,32 +60,34 @@ $resultado = mysqli_query($conexao, $sql);
                     <th scope="col">UF</th>
                     <th scope="col">Email</th>
                     <th scope="col">Ações</th>
-                    
                 </tr>
             </thead>
             <tbody>
-                <?php while($linha = mysqli_fetch_array($resultado)) { ?>
-                <tr>
-                    <td><?= $linha['codigo'] ?></td>
-                    <td><?= $linha['status'] ?></td>
-                    <td><?= $linha['nome'] ?></td>
-                    <td><?= $linha['cpf'] ?></td>
-                    <td><?= $linha['telefone'] ?></td>
-                    <td><?= $linha['endereco'] ?></td>
-                    <td><?= $linha['logradouro'] ?></td>
-                    <td><?= $linha['cep'] ?></td>
-                    <td><?= $linha['cidade']?></td>
-                    <td><?= $linha['uf'] ?></td>
-                    <td><?= $linha['email'] ?></td>
-                    <td>
-                        <a href="cliente-Alterar.php?id=<?= $linha['codigo']?>" class="btn btn-warning"><i class="bi bi-pencil-square"></i></a>
-                        <a href="cliente-Listar.php?id=<?= $linha['codigo'] ?>" class="btn btn-danger" onclick="return confirm('Confirma exclusão?')"><i class="bi bi-trash"></i></a>
-                    </td>
-                </tr>
+                <?php while ($linha = mysqli_fetch_array($resultado)) { ?>
+                    <tr>
+                        <td><?= $linha['codigo'] ?></td>
+                        <td><?= ($linha['status'] == 1) ? 'Ativo' : 'Inativo'?></td>
+                        <td><?= $linha['nome'] ?></td>
+                        <td><?= $linha['cpf'] ?></td>
+                        <td><?= $linha['telefone'] ?></td>
+                        <td><?= $linha['endereco'] ?></td>
+                        <td><?= $linha['logradouro'] ?></td>
+                        <td><?= $linha['cep'] ?></td>
+                        <td><?= $linha['cidade'] ?></td>
+                        <td><?= $linha['uf'] ?></td>
+                        <td><?= $linha['email'] ?></td>
+                        <td class="actions">
+                            <a href="cliente-Alterar.php?id=<?= $linha['codigo'] ?>" class="btn btn-warning">
+                                <i class="fas fa-solid fa-pen-to-square"></i> Alterar</a>
+                            <a href="cliente-Listar.php?id=<?= $linha['codigo'] ?>" class="btn btn-danger"
+                                onclick="return confirm('Confirma exclusão?')"><i class="fas fa-solid fa-trash-can"></i>Excluir</a>
+                        </td>
+                    </tr>
                 <?php } ?>
             </tbody>
         </table>
 
     </div>
 </body>
+
 </html>
