fe84426eb645023d0e141d660963aa1aa87030e2
MarkisPaulo <markispaulo.atanasio3@gmail.com>
2025-10-16 11:59:08 -0300

    agora cadastro de categoria e marca funcionam

--- categoria-cadastrar.php
+++ categoria-cadastrar.php
@@ -5,7 +5,7 @@ if (isset($_POST['cadastrar'])) {
 
     $nome = $_POST['nome'];
 
-$sql = "INSERT INTO marca (nome) VALUES('$nome')";
+    $sql = "INSERT INTO categoria (nome) VALUES('$nome')";
 
     mysqli_query($conexao, $sql);
     echo "Registro salvo com sucesso";
@@ -19,15 +19,15 @@ $sql = "INSERT INTO marca (nome) VALUES('$nome')";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/formCadastro.css">
-    <title>Cadastro de Marca</title>
+    <title>Cadastro de Categoria</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 
 <body>
     <div class="container">
         <div class="form-header">
-            <h1><i class="fas fa-thin fa-registered"></i> Cadastro de Marca</h1>
-            <p>Preencha os dados abaixo para cadastrar um novo produto</p>
+            <h1><i class="fas fa-thin fa-tags"></i> Cadastro de Categoria</h1>
+            <p>Preencha os dados abaixo para cadastrar uma nova categoria</p>
         </div>
 
         <div class="info-box">
@@ -37,13 +37,13 @@ $sql = "INSERT INTO marca (nome) VALUES('$nome')";
         <form>
             <div class="form-group">
                 <label for="nome">Nome*</label>
-                <input type="text" id="nome" placeholder="Digite o nome da Marca" required>
+                <input type="text" id="nome" placeholder="Digite o nome da Categoria" required>
             </div>
            
 
             <div class="button-group">
                 <button type="button" class="btn btn-secondary">Cancelar</button>
-                <button type="submit" class="btn">Cadastrar Marca</button>
+                <button type="submit" class="btn">Cadastrar Categoria</button>
             </div>
         </form>
     </div>
