79a8a9e8ac55f8b42338562352ad878d61b31053
MarkisPaulo <markispaulo.atanasio3@gmail.com>
2025-11-18 10:49:06 -0300

    apenas alterado a lang da página

--- produto-listar.php
+++ produto-listar.php
@@ -11,7 +11,7 @@ $resultado = mysqli_query($conexao, $sql);
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
